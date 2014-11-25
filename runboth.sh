7e935b0662a5f3fe5cedecef1a391e0a7d8d77cd
root <root@beaglebone.(none)>
2014-11-25 17:27:55 +0000

    runboth

---
+++ runboth.sh
@@ -0,0 +1,2 @@
+./aiswatcher -p 7004 -h boatbeaconapp.com -t 1 -P 63  -d  -G 1000  -f /tmp/aisdata
+./aiswatcher -p 7004 -h boatbeaconapp.com -D 1 -F 162025000 -t 1 -P 63  -d  -G 1000  -f /tmp/aisdata
